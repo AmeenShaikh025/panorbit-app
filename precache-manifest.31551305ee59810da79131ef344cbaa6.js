e1acf03b2034c772177e847b3e9cb334d959207f
ameen025 <ameenshaikh25031994@gmail.com>
2020-01-09 17:25:17 +0530

    Updates

---
+++ precache-manifest.31551305ee59810da79131ef344cbaa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad863c4bec17a53f194f04dcc32c801d",
+    "url": "/panorbit-app/index.html"
+  },
+  {
+    "revision": "54344e0b0c3e85bc090f",
+    "url": "/panorbit-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f837dfba00148f026e09",
+    "url": "/panorbit-app/static/js/2.c6819614.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/panorbit-app/static/js/2.c6819614.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54344e0b0c3e85bc090f",
+    "url": "/panorbit-app/static/js/main.96670eab.chunk.js"
+  },
+  {
+    "revision": "8343443c979fb3d32fef",
+    "url": "/panorbit-app/static/js/runtime-main.9d937aca.js"
+  }
+]);
